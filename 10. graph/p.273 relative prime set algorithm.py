6ea04726ee879cdd1900031775d49985f0ddd675
jonghyeon baek <whdgus928@naver.com>
2022-07-13 15:12:08 +0900

    Add files via upload

---
+++ 10. graph/p.273 relative prime set algorithm.py
@@ -0,0 +1,39 @@
+#특정 원소가 속한 집합 찾기
+def find_parent(parent,x):
+    #루트가 아니라면 루트 찾을때까지 재귀 호출
+    if parent[x]!=x:
+        return find_parent(parent,parent[x])
+    return x
+
+#두 원소가 속한 집합 합치기
+def union_parent(parent,a,b):
+    a=find_parent(parent,a)
+    b=find_parent(parent,b)
+    if a<b:
+        parent[b]=a
+    else:
+        parent[a]=b
+
+#노드 개수, 간선 개수 입력받기
+v,e=map(int,input().split())
+#부모 테이블 초기화
+parent=[0]*(v+1)
+
+#부모 테이블에서 부모를 자기 자신으로 초기화
+for i in range(1,v+1):
+    parent[i]=i
+
+#union 연산 수행
+for i in range(e):
+    a,b=map(int,input().split())
+    union_parent(parent,a,b)
+
+print('각 원소가 속한 집합: ', end='')
+for i in range(1,v+1):
+      print(find_parent(parent,i),end=' ')
+
+print()
+
+print('부모테이블: ', end='')
+for i in range(1,v+1):
+    print(parent[i],end=' ')
